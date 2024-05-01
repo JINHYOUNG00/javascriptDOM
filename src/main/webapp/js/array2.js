66241c11d9afa4ae3eea9aab49a3309eb623a5ad
Jinhyoung Kim <jinhyoung0213@gmail.com>
2024-05-01 12:48:36 +0900

    test

--- src/main/webapp/js/array2.js
+++ src/main/webapp/js/array2.js
@@ -4,45 +4,54 @@
 
 let genderAry = []; //gender를 종류별로 한가지만 담기
 empList.forEach(emp => {
+	// 루프돌면서 genderAry 배열에 empList의 gender 속성이 없으면
 	if (genderAry.indexOf(emp.gender) == -1) {
-		genderAry.push(emp.gender);
+		// genderAry 배열에 그 gender를 담아라.
+		genderAry.push(emp.gender);				
 	}
 });
 
 genderAry.forEach(gender => {
+	// option 태그 생성하고 그 변수명을 opt로 선언
 	let opt = document.createElement('option');
-	opt.innerHTML = gender;
+	// <option> genderAry[i] </option>
+	opt.innerHTML = gender;					
+	// <select id="genderList"> <option> genderAry[i] </option> </select> 
 	document.querySelector('#genderList').append(opt);
 });
 
 //함수(배열)
 function makeList(ary = []) {
-	let obj = { id: 1, first_name: '', last_name: '', email: '', gender: '', salary: '' }
+//	let obj = { id: 1, first_name: '', last_name: '', email: '', gender: '', salary: '' }
 	let props = ['id', 'first_name', 'email', 'salary', 'gender'];
+
 
 	ary.forEach(emp => {
 		//한건에 대한 처리.
 		let tr = document.createElement('tr');
+		
 		props.forEach(prop => {
 			let td = document.createElement('td');
 			td.innerHTML = emp[prop];
 			tr.appendChild(td);
-		})
+		}) // end of profs.forEach();
+		
 		document.querySelector('#show tbody').appendChild(tr);
-	});
-};
+		
+	}); // end of ary.forEach();
+}; // end of makeList();
 
 makeList(empList);
 
 let genderSelect = document.querySelector('#genderList');
-let memberList = '';
+
 genderSelect.addEventListener('change', changeGender);
 
 
 function changeGender() {
 	document.querySelector('tbody').innerHTML = null;
 	let memberList = empList.filter((item, idx, ary) => {
-		return item.gender == document.querySelector('#genderList').options[document.querySelector('#genderList').selectedIndex].value;
+		return item.gender == genderSelect.options[genderSelect.selectedIndex].value;
 	});
 	return makeList(memberList);
 }
